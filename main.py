76f882628e9d58e1bed4cf032d3e0dbe30a3d79a
Chisoo Yang <danics7@users.noreply.github.com>
2022-02-03 15:37:14 +0900

    폰트 변경 및 패치파일 수정

--- main.py
+++ main.py
@@ -31,6 +31,22 @@ if __name__ == '__main__':
     translations = lang['translations']
     for row in translations:
         codes = codes.replace(row['original'], row['korean'])
+    codes = codes.replace('Arial, Helvetica, sans-serif', '메이플스토리')
+    f = open(newPath, 'w', encoding='UTF-8')
+    f.write(codes)
+    f.close()
+    shutil.move(newPath, originalPath)
+
+    # 폰트변경
+    originalPath = '.\\resources\\app\\.webpack\\renderer\\vendors.bundle.js'
+    newPath = '.\\resources\\app\\.webpack\\renderer\\vendors.bundle.js.new'
+    f = open(originalPath, 'r', encoding='UTF8')
+    originalCodes = f.read()
+    codes = originalCodes
+    f.close()
+
+    codes = codes.replace('Courier', '메이플스토리')
+
     f = open(newPath, 'w', encoding='UTF-8')
     f.write(codes)
     f.close()
@@ -52,5 +68,6 @@ if __name__ == '__main__':
         f.close()
         shutil.move(langNewPath, langOriginalPath)
 
-    print("main.bundle.js 변경. %d개 lang파일 변경.\r\n패치가 안되었다면 무결성검사 후 다시 실행해주세요~" % len(lang['lang']))
+    print("main.bundle.js, vendor.bundle.js(폰트수정) 변경.\r\n%d개 lang파일 변경.\r\n패치가 안되었다면 무결성검사 후 다시 실행해주세요~\r\n" % len(lang['lang']))
+    print("폰트는 메이플스토리체로 변경되었으며, 같이 압축된 Maplestory Light.ttf 파일을 실행해서 설치해주세요.")
     close()
--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
             "icon_resources": [(1, "0.ico")],
             "dest_base": "Vampire Survivors 한글패치",
             "name": "Vampire Survivors 한글패치",
-            "version": "0.2.9.3"
+            "version": "0.2.10.1"
         }],
-    version='0.2.9.3',
+    version='0.2.10.1',
     name='Vampire Survivors 한글패치',
     description='Vampire Survivors 한글패치',
     author='danics'
